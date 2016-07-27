cd8f0c2500582831bd63b0d0c93cafaa30e42eeb
Love4653 <Kaiya.park@gmail.com>
2016-07-27 12:05:36 -0700

    AAA

--- src/WorldDomination.java
+++ src/WorldDomination.java
@@ -1,4 +1,20 @@
+import javax.swing.JOptionPane;
 
 public class WorldDomination {
+	public static void main(String[] args) {
+		// 1. Ask the user if they know how to write code.
+		String CodeBackground = JOptionPane.showInputDialog("Do you know how to write code?");
 
-}
+		// 2. If they say "yes", tell them they will rule the world.
+		if (CodeBackground.equals("yes")){
+			JOptionPane.showMessageDialog(null, "Then, you will rule the world");
+		}
+
+		// 3. Otherwise, wish them good luck washing dishes.
+
+	}else
+
+	{
+		JOptionPane.showMessageDialog(null, "Them, good luck washing filthy dishes");
+	}
+}}
