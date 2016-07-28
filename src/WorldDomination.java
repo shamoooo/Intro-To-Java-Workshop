09aa146eb393a89102599c14224b838cf9d4cf94
Love4653 <Kaiya.park@gmail.com>
2016-07-28 12:01:40 -0700

    BBB

--- src/WorldDomination.java
+++ src/WorldDomination.java
@@ -6,15 +6,15 @@ public class WorldDomination {
 		String CodeBackground = JOptionPane.showInputDialog("Do you know how to write code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
-		if (CodeBackground.equals("yes")){
+		if (CodeBackground.equals("yes")) {
 			JOptionPane.showMessageDialog(null, "Then, you will rule the world");
 		}
 
 		// 3. Otherwise, wish them good luck washing dishes.
 
-	}else
-
-	{
-		JOptionPane.showMessageDialog(null, "Them, good luck washing filthy dishes");
+		else {
+			JOptionPane.showMessageDialog(null, "Then, good luck washing filthy dishes");
+		}
 	}
-}}
+
+}
